63cd5593dc95011b2fd4d058cac20806c3892afa
Murkiwater <kekys492@gmail.com>
2025-05-15 04:13:30 +0500

    update and delete vi roflite a ya eto 4 dnya pisal

--- mysite/articles/urls.py
+++ mysite/articles/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from articles import views
 
 app_name = 'articles'
@@ -7,5 +6,7 @@ app_name = 'articles'
 urlpatterns = [
     path('', views.article_list, name="articles"),
     path('create', views.article_create, name='create'),
-    path('<slug:slug>', views.article_item, name='article-detail')
+    path('<slug:slug>', views.article_item, name='article-detail'),
+    path('update/<slug:slug>/', views.article_update, name='update'),
+    path('delete/<slug:slug>/', views.article_delete, name='delete'),
 ]
